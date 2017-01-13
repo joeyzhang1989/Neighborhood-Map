c22049a7c97982eb915eab4a61a64f181d78fe8b
chenglong zhang <chenglongzhang931@gmail.com>
2017-01-12 21:39:06 -0400

    put all js code in one place, replace for-in with for and bind error messages to UI

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
+//detect the mobile device
+$(document).ready(function() {
+    var isMobile = false;
+    var windowSize = $(window).width();
+    // device detection
+    if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4)) || windowSize <= 900) {
+        isMobile = true;
+        if (isMobile === true) {
+            $("head").append('<link rel="stylesheet" href="css/mobile.css"/>');
+        }
+    }
+});
+
 // Model for the nearBy place markers based on the neighborhood location
 var venueMarkers = function(item) {
     this.name = ko.observable(item.venue.name);
@@ -136,7 +149,9 @@ var mapViewModel = function() {
     var markers = [];
     var defaultNeighborhood = '3455 Chemin de la Côte-des-Neiges, Montréal, QC H3H, Canada';
     var neighborhoodMarker = []; // create a blank array to store the makers
-    self.neighborhood = ko.observable('');
+    self.neighborhood = ko.observable(''); //update the neighborhood location
+    self.neighborhoodErrorMessage = ko.observable(''); // show on UI when load default neoghborhood fail
+    self.fourSquareErrorMessage = ko.observable(''); // show on UI when load fourSquare fail
     self.message = ko.observable('Set new neighborhood location');
     self.fliteredMessage = ko.observable('Fliter by the name');
     self.keyword = ko.observable('');
@@ -166,9 +181,6 @@ var mapViewModel = function() {
             self.neighborhood('');
         }
     });
-    // initial the map object
-    // initMap();
-
     // center the map when the window resize
     google.maps.event.addDomListener(window, "resize", function() {
         var center = map.getCenter();
@@ -180,7 +192,6 @@ var mapViewModel = function() {
         $("#map").height($(window).height());
         $("#map").width($(window).width());
     });
-
 
     setDefaultNeighborhood(defaultNeighborhood);
     autocompletion();
@@ -222,8 +233,8 @@ var mapViewModel = function() {
             createMarkersForNeighborhood(results[0]);
             getNeiborhoodInformation(results[0]);
         } else {
-            $('.places').html('failed to load the defaultNeighborhood');
-            console.log('failed to load the defaultNeighborhood');
+            self.neighborhoodErrorMessage('Fail to load the default Neighborhood, please refresh');
+            console.log('Failed to load the defaultNeighborhood');
         }
     }
     // set the locaion, titie and icon for the neighborhood marker
@@ -288,8 +299,8 @@ var mapViewModel = function() {
                 // });
             }
         }).fail(function(e) {
-            $('.places').html('failed to load foursquare json');
-            console.log('failed to load foursquare json');
+            self.fourSquareErrorMessage('Failed to load foursquare json,please refresh')
+            console.log('Failed to load foursquare json');
         });
     }
     // create the markers use the info returned by the foursquare API
@@ -358,11 +369,11 @@ var mapViewModel = function() {
     function stopAnimation(marker) {
         setTimeout(function() {
             marker.setAnimation(null);
-        }, 3000);
+        }, 2100);
     }
     // remove neighborhood marker from the map
     function removeNeighborhoodMarker() {
-        for (var i in neighborhoodMarker) {
+        for (var i = 0, l = neighborhoodMarker.length; i < l; i++) {
             neighborhoodMarker[i].setMap(null);
             neighborhoodMarker[i] = null;
         }
@@ -370,11 +381,11 @@ var mapViewModel = function() {
             neighborhoodMarker.pop();
         }
     }
-    // remove the markers when the new neighborhood loaction is set
+    // remove the venueMarkers when the new neighborhood loaction is set
     function removeMarker() {
-        for (var place in markers) {
-            markers[place].setMap(null);
-            markers[place] = null;
+        for (var i = 0, l = markers.length; i < l; i++) {
+            markers[i].setMap(null);
+            markers[i] = null;
         }
         while (markers.length > 0) {
             markers.pop();
@@ -410,7 +421,7 @@ var mapViewModel = function() {
             //Loop through the markers array and see if the search keyword matches
             //with any venue name, if so push that object to the filteredList
             //array and place the marker on the map
-            for (var place in self.nearByPlaces()) {
+            for (var place = 0, l = self.nearByPlaces().length; place < l; place++) {
                 if (self.nearByPlaces()[place].name().toLowerCase().indexOf(searchWord) !== -1) {
                     self.filteredList.push(self.nearByPlaces()[place]);
                     markers[place].setMap(map);
@@ -433,7 +444,8 @@ var mapViewModel = function() {
     self.clearFilter = function() {
         self.filteredList(self.nearByPlaces());
         self.fliteredMessage('Fliter by the name');
-        for (var marker in markers) {
+        place
+        for (var marker = 0, l = markers.length; marker < l; marker++) {
             markers[marker].setMap(map);
         }
     };
